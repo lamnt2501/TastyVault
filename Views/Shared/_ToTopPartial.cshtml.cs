9b11a9cf6a1e3a9123044a3ce4d8cee8adafcabf
lamnt2501 <trung.lam.for.work@gmail.com>
2023-05-29 00:29:10 +0700

    sửa giao diện trang chi tiết công thức responsive trang home

---
+++ Views/Shared/_ToTopPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TastyVault.Views.Shared
+{
+    public class _ToTopPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
