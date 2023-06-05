c0e61ded6b972ae0e450936b933bdbee612c665e
lamnt2501 <trung.lam.for.work@gmail.com>
2023-06-06 01:08:33 +0700

    hoàn tất đổ dữ liệu trang home

--- Views/Shared/_HotBlog.cshtml.cs
+++ Views/Shared/_HotBlog.cshtml.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TastyVault.Models;
 
 namespace TastyVault.Views.Shared
 {
   public class _HotBlogModel : PageModel
   {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string ImagePath { get; set; }
     public void OnGet()
     {
 
